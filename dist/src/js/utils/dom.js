96bcdd8d27f29e37492955965c6b31dfa3480296
Norwiee <fc288d5y1725@student.devacademy.id>
2025-06-09 22:34:34 +0700

    deploy dist

--- dist/src/js/utils/dom.js
+++
@@ -1,8 +0,0 @@
-// src/js/utils/dom.js
-export function createElement(tag, attributes = {}) {
-  const element = document.createElement(tag);
-  Object.entries(attributes).forEach(([key, value]) => {
-    element.setAttribute(key, value);
-  });
-  return element;
-}
--- dist/src/js/utils/transition.js
+++
@@ -1,9 +0,0 @@
-// src/js/utils/transition.js
-export function navigateWithTransition(callback) {
-  const content = document.getElementById("content");
-  content.style.opacity = "0";
-  setTimeout(() => {
-    callback();
-    content.style.opacity = "1";
-  }, 300);
-}
